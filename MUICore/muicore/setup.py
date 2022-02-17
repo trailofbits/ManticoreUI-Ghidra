76476b08a4450bce3c71266baac8a81b764b033e
kokrui <kokruiwong@gmail.com>
2022-02-17 00:31:32 -0800

    update binary, include shiv build util script

--- MUICore/muicore/setup.py
+++ MUICore/muicore/setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='muicore',
     version='0.0.1',
-    py_modules=["mui_server", "MUICore_pb2_grpc", "MUICore_pb2"],
+    py_modules=["mui_server", "MUICore_pb2_grpc", "MUICore_pb2", "introspect_plugin"],
     entry_points={
         "console_scripts": [
             "muicore=mui_server:main",
