b414b5090e16ee21aeeb119135d819ca6ab81f6e
kokrui <kokruiwong@gmail.com>
2022-02-23 07:56:12 -0800

    change install/uninstall dir, bump gRPC version

--- MUI/src/main/grpc/muicore/ManticoreUIGrpc.java
+++ MUI/src/main/grpc/muicore/ManticoreUIGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.44.0)",
+    value = "by gRPC proto compiler (version 1.44.1)",
     comments = "Source: MUICore.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class ManticoreUIGrpc {
